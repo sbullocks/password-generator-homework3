2c24dd1e7f8a38aa663eada7ad613a21b2b41389
sbullocks <sbullocks@gmail.com>
2022-10-01 00:56:42 -0400

    added arrays for each of the prompts.

--- Develop/script.js
+++ Develop/script.js
@@ -27,6 +27,13 @@ function generatePassword() {
   var userWantsUppercase = window.confirm("Would you like to include uppercase letters in your password?")
   var userWantsLowercase = window.confirm("Would you like to include lowercase letters in your password?")
 
+//Creeated array to include all categories above.
+var numberList = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+var symbolList = ["~", "`", "!",  "@", "#", "$", "%", "^", "&", "*", "()", "_", "-", "+", "=", "{", "[", "}", "]", "|"]
+var lowercaseList = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+var uppercaseList = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"] 
+var randomList = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "~", "`", "!",  "@", "#", "$", "%", "^", "&", "*", "()", "_", "-", "+", "=", "{", "[", "}", "]", "|", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword(); 
