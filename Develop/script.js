06f0e998f31f1b25376225f2ec37f89906989a7f
sbullocks <sbullocks@gmail.com>
2022-10-01 00:53:50 -0400

    added password lenght

--- Develop/script.js
+++ Develop/script.js
@@ -10,6 +10,18 @@ function generatePassword() {
 
   var passwordLength = parseInt(userInput) //This will allow a "sting" to be passed and read as a number. If cannot be read as number, console log will return NaN (not a number).
 
+  if (isNaN(passwordLength)) { //If passwordLength is true, then it will result in a number. IF it is a number, it will move to the next else or else if.. if there are any more.
+    window.alert("That's not a number!")
+    return //If the input is not a number (false), then this will end the prompt. It will not go to the next statement. It will return to the call function (var password = generatePassword();)
+  } //else { //This else statement is technically not needed because if the above is (false), it will return to the original prompt.
+    //window.alert("That is a valid number!") 
+  //}
+
+  if (passwordLength < 8 || passwordLength > 128) {
+    window.alert("Password length must be between 8 and 128 characters.")
+    return
+  }
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword(); 
